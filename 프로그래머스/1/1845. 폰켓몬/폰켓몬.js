20b19827ec246c84be6b6d17be5852d717b37f6d
keeprok <140785214+keeprok@users.noreply.github.com>
2025-11-19 21:15:01 +0900

    [level 1] Title: 폰켓몬, Time: 0.57 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,4 @@
 function solution(nums) {
-  const maxPick = nums.length / 2;     // 절반만 선택 가능
-  const kinds = new Set(nums).size;    // 중복 제거 후 종류 수
-  
-  return Math.min(maxPick, kinds);
+    var answer = 0;
+    return answer;
 }
