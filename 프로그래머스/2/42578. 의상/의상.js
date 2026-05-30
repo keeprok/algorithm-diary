ab1c625e32059b6cab992e851f87d8e8e027f962
keeprok <140785214+keeprok@users.noreply.github.com>
2026-05-30 23:50:40 +0900

    [level 2] Title: 의상, Time: 0.18 ms, Memory: 32.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,19 +1,15 @@
 function solution(clothes) {
-  let hash = {};
-
-  for (let [name, type] of clothes) {
-    if (!hash[type]) hash[type] = 0;
-    hash[type]++;
-  }
-
-  let result = 1;
+  const map = new Map()
+    for(const [name , type] of clothes){
+        map.set(type , (map.get(type)??0)+1)
+    }
+    let answer =1 
+    for (const count of map.values()){
+        answer *= count+1
+    }
+    return answer-1
     
-  for (let type in hash) {
-    result *= (hash[type] + 1); 
-  }
-
-  return result - 1;
 }
 
-// 1. 모자 (0~n) 상의 (0~n)이런식으로 총4종류 n+1 각 종류객체 만들기 
+// 1. 모자 (0~n) 상의 (0~n)이런식으로 총4종류 n+1 각 종류객체 만들기
 // 2. 각개체 곱하기 단 전체 0일땐 빼기
