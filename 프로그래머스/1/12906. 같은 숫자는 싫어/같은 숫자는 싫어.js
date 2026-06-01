1cb70a13536decad4731f6a4fab8f885d15014df
keeprok <140785214+keeprok@users.noreply.github.com>
2026-06-01 23:41:29 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 26.07 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,11 +1,13 @@
-function solution(arr) {
-  const stack = [];
-
-  for (const x of arr) {
-    if (stack.length === 0 || stack[stack.length - 1] !== x) {
-      stack.push(x);
-    }
-  }
-
-  return stack;
+function solution(arr)
+{
+ const answer = []
+ 
+ for (let num of arr){
+     if(answer.length == 0 || answer[answer.length-1] !== num ){
+         answer.push(num)
+     }
+ }
+    return answer
 }
+// 1. 결과 배열 answer를 만든다
+// 2. arr를 돌면서, answer 맨 뒤와 지금 숫자가 같으면 넣지 않고, 다르면 넣는다
