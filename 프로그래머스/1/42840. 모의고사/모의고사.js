a5a88d8c6146b00069dd06e2eee4fdc19a228101
keeprok <140785214+keeprok@users.noreply.github.com>
2026-06-02 15:41:20 +0900

    [level 1] Title: 모의고사, Time: 2.59 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,24 +1,23 @@
 function solution(answers) {
-    const fir =[1,2,3,4,5]
-    const sec =[2, 1, 2, 3, 2, 4, 2, 5]
-    const third = [3,3,1,1,2,2,4,4,5,5]
-    const scores = [0,0,0]
-    // 정수화
-     for (let i = 0; i < answers.length; i++) {
-    if (answers[i] === fir[i % fir.length]) scores[0]++;
-    if (answers[i] === sec[i % sec.length]) scores[1]++;
-    if (answers[i] === third[i % third.length]) scores[2]++;
-  }//반복 문
+    const one = [1,2,3,4,5]
+    const two = [2, 1, 2, 3, 2, 4, 2, 5]
+    const three = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    let scores = [0,0,0]
     
-    const maxScore = Math.max(...scores)
-    //최댓값
-    
-    const result = []
-    for (let i = 0; i < scores.length; i++) { 
-        if (scores[i] === maxScore) result.push(i + 1) 
-    }//결과값
+    for(let i = 0 ; i<answers.length ; i++ ){
+        if(answers[i] === one[i %one.length])scores[0]++;
+        if(answers[i] === two[i %two.length])scores[1]++;
+        if(answers[i] === three[i %three.length])scores[2]++;
+    }
+   const maxScore = Math.max(...scores)
+   
+   const result = []
+    for(let i =0 ;i < scores.length ; i++){
+        if(scores[i]===maxScore)result.push(i+1)
+    }   
     
     return result;
 }
-// 1. 첫번째 인원 부터 3번째 인원배열 정수화 ex 1=[1,2,3,4,5] ,2= [2, 1, 2, 3, 2, 4, 2, 5],3 =[3,3,1,1,2,2,4,4,5,5]
-// 2. 입력받는 수랑 정수 비교 및 맞춘개수 3개 배열 비교  및 가장많이맞춘 번호 저장 단 동일시 오름차순
+// 1. 각 수포자의 반복 패턴 배열과 점수(scores)를 만든다
+// 2. answers를 순회하면서 문제번호 % 패턴길이로 각 수포자 답을 비교해 점수를 올린다
+// 3. scores에서 최대 점수를 구하고, 최대점수와 같은 수포자 번호를 answer에 담아 return
