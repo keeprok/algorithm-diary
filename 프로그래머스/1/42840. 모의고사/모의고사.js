9654e4b5504ec4b87b5942289e20e511d1b934fc
keeprok <140785214+keeprok@users.noreply.github.com>
2026-06-02 16:02:39 +0900

    [level 1] Title: 모의고사, Time: 3.72 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,22 +1,20 @@
 function solution(answers) {
-    const one = [1,2,3,4,5]
-    const two = [2, 1, 2, 3, 2, 4, 2, 5]
-    const three = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    const first = [1,2,3,4,5]
+    const second = [2, 1, 2, 3, 2, 4, 2, 5]
+    const third = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     let scores = [0,0,0]
-    
-    for(let i = 0 ; i<answers.length ; i++ ){
-        if(answers[i] === one[i %one.length])scores[0]++;
-        if(answers[i] === two[i %two.length])scores[1]++;
-        if(answers[i] === three[i %three.length])scores[2]++;
+    for (let i = 0 ; i< answers.length; i++ ){
+        if(answers[i] === first[i%first.length])scores[0]++
+        if(answers[i] === second[i%second.length])scores[1]++
+        if(answers[i] === third[i%third.length])scores[2]++
     }
-   const maxScore = Math.max(...scores)
-   
-   const result = []
-    for(let i =0 ;i < scores.length ; i++){
-        if(scores[i]===maxScore)result.push(i+1)
-    }   
-    
-    return result;
+    let result = []
+    const maxScores =   Math.max(...scores)
+    for (let i= 0 ; i <scores.length ; i++){
+        if(scores[i]=== maxScores)result.push(i+1)
+    }
+    return result
+  
 }
 // 1. 각 수포자의 반복 패턴 배열과 점수(scores)를 만든다
 // 2. answers를 순회하면서 문제번호 % 패턴길이로 각 수포자 답을 비교해 점수를 올린다
