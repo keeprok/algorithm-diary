7268b2de12a14c7640b333c8b56bb55244846e79
keeprok <140785214+keeprok@users.noreply.github.com>
2026-06-02 16:49:01 +0900

    [level 2] Title: 카펫, Time: 0.17 ms, Memory: 32.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,14 +1,12 @@
 function solution(brown, yellow) {
-  // yellow의 약수 쌍 (a, b)을 찾는다. (a >= b)
-  for (let b = 1; b * b <= yellow; b++) {
-    if (yellow % b !== 0) continue;
-
-    const a = yellow / b;     
-    const W = a + 2;
-    const H = b + 2;
-
-    if (W * H === brown + yellow) {
-      return [W, H];
-    }
+  const total = brown + yellow
+  for (let h =3 ; h<= Math.sqrt(total); h++){
+      const w = total/h
+      if( w < h)continue
+      if (2 * w + 2 * h - 4 === brown){
+          return [w ,h]
+      }
   }
 }
+//1 . 갈색 은 테두리를 포함하다고 생각하고 2w +2h -4 = 테두리 
+//2 . 갈색 + 노란색은 w*h  단 w>= h 이다 
