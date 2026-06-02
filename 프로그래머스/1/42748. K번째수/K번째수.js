e9dcee97240409f9ed30a41b7cf9f4cb5794bb5b
keeprok <140785214+keeprok@users.noreply.github.com>
2026-06-02 14:38:19 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 32.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,7 +1,15 @@
 function solution(array, commands) {
-  return commands.map(([i, j, k]) => {
-    const sliced = array.slice(i - 1, j);     
-    sliced.sort((a, b) => a - b);              
-    return sliced[k - 1];                     
-  });
+  const answer = []
+  
+  for(const [i , j , k] of commands){
+    const  sliced = array.slice(i-1,j)
+    const sorted = sliced.sort((a,b)=>a-b)
+    answer.push(sorted[k-1])
+  }
+  
+  return answer
 }
+// 0. answer 배열 생성
+// 1.  array 에서 commands 배열안 순서대로 확인을해서 배열안배열에서 i번째에서 j번째까지 추출
+// 2. sort min 으로 최소단위로 정렬을 한뒤 거기서부터 k 번째의 수를  answer에 push 
+// 3. 반복후 return
