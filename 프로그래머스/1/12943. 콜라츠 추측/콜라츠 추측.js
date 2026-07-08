297b2c2be2fe5ff663a0ed6ec438b28a96a34e1e
keeprok <140785214+keeprok@users.noreply.github.com>
2026-07-08 13:37:46 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 44 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,12 +1,19 @@
 function solution(num) {
-  let cnt = 0;
-  let x = num;
-
-  while (x !== 1 && cnt < 500) {
-    if (x % 2 === 0) x = x / 2;
-    else x = x * 3 + 1;
-    cnt++;
-  }
-
-  return x === 1 ? cnt : -1;
+    let answer = 0;
+    
+   while(num !== 1 && answer < 500) {
+       if(num%2 == 0){
+          num = num/2
+       }else {
+           num = num*3 + 1
+       }
+       answer ++
 }
+   
+    if (num !== 1) {  
+    return -1
+    }
+    return answer
+}
+// 1. 언제까지 반복되는지 정의를 미리 할수없기때문에 while을 쓴다
+// 2. num 이 1이되거나 answer이 =>500 이되면 멈춘다 
